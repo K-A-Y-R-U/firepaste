3027de330cfaadd56a03dae126e8ad12fae06bd3
k a y r u <nestor.daniel.22x@gmail.com>
2026-04-04 10:31:21 -0500

    fix: tailwind preset filament y filament css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
-/** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -10,6 +10,7 @@ export default {
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
         './vendor/awcodes/filament-tiptap-editor/resources/**/*.blade.php',
+        './vendor/awcodes/filament-tiptap-editor/resources/**/*.js',
         'node_modules/preline/dist/*.js',
     ],
     theme: {
@@ -22,8 +23,5 @@ export default {
     plugins: [
         require('preline/plugin'),
         require('@tailwindcss/forms'),
-        require('@tailwindcss/nesting'),
-        require('tailwindcss'),
-        require('autoprefixer'),
     ],
-};
+}
